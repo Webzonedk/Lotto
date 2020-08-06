3c5718380310712ebd1c010e0c1a66edbade0ca2
Webzonedk <webzone@kpdesign.dk>
2020-08-06 20:49:30 +0200

    Add project files.

---
+++ Methods.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lotto
+{
+    class Methods
+    {
+        /// <summary>
+        /// Generates the random lotto winner numbers, and ensures that the numbers are only picked once
+        /// </summary>
+        /// <param name="newLottonumbers"></param>
+        /// <returns></returns>
+        public static int[] RandomLottoNumbersGenerator(int[] newLottonumbers)
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < newLottonumbers.Length; i++)
+            {
+                int randomNumber = random.Next(1, 21);
+
+                for (int j = 0; j < newLottonumbers.Length; j++)
+                {
+                    if (randomNumber == newLottonumbers[j])
+                    {
+                        i--;
+                        j = 7;
+                    }
+                    else if (randomNumber != newLottonumbers[j] && j == 6)
+                    {
+                        newLottonumbers[i] = randomNumber;
+
+                    }
+
+                }
+            }
+            return newLottonumbers;
+        }
+
+
+
+
+        /// <summary>
+        /// Creating User coupon from numbers chosen by the user
+        /// and compares them with every time the number is chosen to ensure that it is a number
+        /// and it is within the range, and it is not already used
+        /// </summary>
+        /// <param name="userCoupon"></param>
+        /// <returns></returns>
+        public static int[] UserCoupon(int[] userCoupon)
+        {
+            for (int i = 0; i < userCoupon.Length; i++)
+            {
+                try
+                {
+                    int tempNumber = int.Parse(Console.ReadLine());
+                    for (int j = 0; j < userCoupon.Length; j++)
+                    {
+                        if (tempNumber == userCoupon[j])
+                        {
+                            i--;
+                            j = 7;
+                            Console.WriteLine("You cant choose the same number twice");
+                        }
+                        else if (tempNumber < 1 || tempNumber > 20)
+                        {
+                            Console.WriteLine("You need to choose numbers between 1 and 20");
+                            i--;
+                            j = 7;
+                        }
+                        else if (tempNumber != userCoupon[j] && tempNumber >= 1 && tempNumber <= 20 && j == 6)
+                        {
+                            userCoupon[i] = tempNumber;
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("You need to pick a number needs to be between 1 and 20");
+                    i--;
+                }
+            }
+            return userCoupon;
+        }
+
+
+
+
+        /// <summary>
+        /// Compares the picked numbers by the user, with the weeks picked numbers by the random generator, '
+        /// to see if the user is a winner.
+        /// </summary>
+        /// <param name="lottoNumbers"></param>
+        /// <param name="userNumbers"></param>
+        /// <returns></returns>
+        public static int[] CompareNumbers(int[] winnerNumbersAmount, int[] lottoNumbers, int[] userNumbers)
+        {
+           int tempWinnerAmount = 0;
+            for (int i = 0; i < userNumbers.Length; i++)
+            {
+                for (int j = 0; j < userNumbers.Length; j++)
+                {
+                    if (userNumbers[i] == lottoNumbers[j])
+                    {
+                        tempWinnerAmount++;
+                        winnerNumbersAmount[0]= tempWinnerAmount;
+                        j = userNumbers.Length;
+                    }
+                }
+            }
+          return winnerNumbersAmount;
+        }
+
+
+
+
+        /// <summary>
+        /// Calculating the winning price, based on amount of correct numbers
+        /// </summary>
+        /// <param name="winnerNumbersAmount"></param>
+        /// <param name="winnerPrice"></param>
+        /// <returns></returns>
+        public static int[] CalculatePrice(int[] winnerNumbersAmount, int[] winnerPrice)
+        {
+            switch (winnerNumbersAmount[0])
+            {
+                case 0:
+                    winnerPrice[0] = 0;
+                    break;
+                case 1:
+                    winnerPrice[0] = 0;
+                    break;
+                case 2:
+                    winnerPrice[0] = 42;
+                    break;
+                case 3:
+                    winnerPrice[0] = 219;
+                    break;
+                case 4:
+                    winnerPrice[0] = 2195;
+                    break;
+                case 5:
+                    winnerPrice[0] = 46325;
+                    break;
+                case 6:
+                    winnerPrice[0] = 159357;
+                    break;
+                case 7:
+                    winnerPrice[0] = 120963254;
+                    break;
+            }
+            return winnerPrice;
+        }
+    }
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lotto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool run = true;
+            while (run)
+            {
+                Console.Clear();
+                int[] lottonumbers = new int[7];
+                int[] userCoupon = new int[7];
+                int[] winnerNumbersAmount = new int[1];
+                int[] winnerPrice = new int[1];
+
+                Console.WriteLine("Welcome to the Lotto lottery");
+
+                Console.WriteLine("Choose your 7 numbers between 1 and 20");
+
+
+
+
+                Methods.UserCoupon(userCoupon);
+
+                Methods.RandomLottoNumbersGenerator(lottonumbers);
+
+                Methods.CompareNumbers(winnerNumbersAmount, lottonumbers, userCoupon);
+
+                Methods.CalculatePrice(winnerNumbersAmount, winnerPrice);
+
+
+
+
+                Console.WriteLine("This weeks lottonumbers");
+                for (int i = 0; i < lottonumbers.Length; i++)
+                {
+                    Console.Write($"{lottonumbers[i],4}");
+                }
+                Console.WriteLine("\n");
+
+
+
+
+                Console.WriteLine("You have chosen the following 7 numbers.");
+                for (int i = 0; i < userCoupon.Length; i++)
+                {
+                    Console.Write($"{userCoupon[i],4}");
+                }
+                Console.WriteLine("\n");
+
+
+
+
+                if (winnerNumbersAmount[0] < 1)
+                {
+                    Console.WriteLine($"You have {winnerNumbersAmount[0]} numbers correct. You might be the most unlucky person in the world.");
+                }
+                else if (winnerNumbersAmount[0] == 1)
+                {
+                    Console.WriteLine($"You have {winnerNumbersAmount[0]} number correct. Unfortunately that is not enough to win the lottery.");
+                }
+                else if (winnerNumbersAmount[0] > 1)
+                {
+                    Console.WriteLine($"Congratulation. You have {winnerNumbersAmount[0]} numbers correct at your lotto coupon. You have won {winnerPrice[0]} $ in the lotto lottery");
+                }
+
+
+
+
+                Console.WriteLine("Try again press 1 | end game: pres anything else");
+                string choose = Console.ReadLine();
+                switch (choose)
+                {
+                    case "1":
+                        break;
+                    default:
+                        run = false;
+                        break;
+                }
+            }
+        }
+    }
+}
